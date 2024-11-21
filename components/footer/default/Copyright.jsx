65889946c1987e3f53f945de78ed664624b5f115
Bunmi Oye <bunmioye09@gmail.com>
2024-11-21 17:59:04 +0100

    clean up footer

--- components/footer/default/Copyright.jsx
+++ components/footer/default/Copyright.jsx
@@ -7,16 +7,7 @@ const Copyright = () => {
         <div className="row x-gap-30 y-gap-10">
           <div className="col-auto">
             <div className="d-flex items-center">
-              © {new Date().getFullYear()} by
-              <a
-                href="https://themeforest.net/user/ib-themes"
-                className="mx-2"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                ib-themes
-              </a>
-              All rights reserved.
+              © {new Date().getFullYear()} by Boxtrip. All rights reserved.
             </div>
           </div>
           {/* End .col */}
@@ -36,7 +27,7 @@ const Copyright = () => {
 
       <div className="col-auto">
         <div className="row y-gap-10 items-center">
-          <div className="col-auto">
+          {/* <div className="col-auto">
             <div className="d-flex items-center">
               <button className="d-flex items-center text-14 fw-500 text-dark-1 mr-10">
                 <i className="icon-globe text-16 mr-10" />
@@ -47,7 +38,7 @@ const Copyright = () => {
                 <span className="underline">USD</span>
               </button>
             </div>
-          </div>
+          </div> */}
           {/* End .col */}
 
           <div className="col-auto">
